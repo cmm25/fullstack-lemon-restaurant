3664e0f76fadd1daa1e73780d9fa9e770cb2663d
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2023-12-29 01:12:49 +0300

    models and serializers added

--- myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from myapp.forms import MenuForm
-from .models import Menu
-from django.http import JsonResponse
-
-# Add code for form_view() function below
-
-def form_view(request):
-    pass
